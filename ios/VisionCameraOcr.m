8f2a85678df3989a8fbde486770ac86b7dd54db2
vteccb <vteccb@gmail.com>
2025-06-20 00:00:12 +0300

    Fixed ios imports

--- ios/VisionCameraOcr.m
+++ ios/VisionCameraOcr.m
@@ -1,5 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <VisionCamera/FrameProcessorPlugin.h>
+#import <VisionCamera/FrameProcessorPluginRegistry.h>
+#import <VisionCamera/Frame.h>
 
 @interface VISION_EXPORT_SWIFT_FRAME_PROCESSOR(scanOCR, OCRFrameProcessorPlugin)
 @end
