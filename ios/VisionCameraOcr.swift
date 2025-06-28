c80c9d0d4b30f9803d389bde4b849013588c6160
vteccb <vteccb@gmail.com>
2025-06-28 17:38:44 +0300

    Fixed ios orientation

--- ios/VisionCameraOcr.swift
+++ ios/VisionCameraOcr.swift
@@ -2,24 +2,57 @@ import Foundation
 import VisionCamera
 import MLKitVision
 import MLKitTextRecognition
+import CoreMotion
+import UIKit
 
 @objc(OCRFrameProcessorPlugin)
 public class OCRFrameProcessorPlugin: FrameProcessorPlugin {
 
     private static var textRecognizer = TextRecognizer.textRecognizer(options: TextRecognizerOptions())
+    private var orientationManager = OCROrientationManager()
+    private let ciContext = CIContext()
 
     public override init(proxy: VisionCameraProxyHolder, options: [AnyHashable : Any]! = [:]) {
         super.init(proxy: proxy, options: options)
     }
 
+    func getImageOrientation() -> UIImage.Orientation {
+        switch orientationManager.orientation {
+        case .portrait:
+            return .right
+        case .landscapeLeft:
+            return .up
+        case .portraitUpsideDown:
+            return .left
+        case .landscapeRight:
+            return .down
+        @unknown default:
+            return .up
+        }
+    }
+
     public override func callback(_ frame: Frame, withArguments arguments: [AnyHashable : Any]?) -> Any {
 
-        guard let buffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
+        guard let imageBuffer = CMSampleBufferGetImageBuffer(frame.buffer) else {
             return ["error": "Failed to get image buffer"]
         }
 
-        let visionImage = VisionImage(buffer: frame.buffer)
-        visionImage.orientation = frame.orientation
+        var ciImage = CIImage(cvPixelBuffer: imageBuffer)
+
+        if frame.isMirrored {
+            let flipTransform = CGAffineTransform(scaleX: -1, y: 1)
+            let translateTransform = CGAffineTransform(translationX: ciImage.extent.width, y: 0)
+            ciImage = ciImage.transformed(by: flipTransform.concatenating(translateTransform))
+        }
+
+        guard let cgImage = ciContext.createCGImage(ciImage, from: ciImage.extent) else {
+            return ["error": "Failed to create CGImage"]
+        }
+
+        let uiImage = UIImage(cgImage: cgImage)
+
+        let visionImage = VisionImage(image: uiImage)
+        visionImage.orientation = getImageOrientation()
 
         do {
             let result = try Self.textRecognizer.results(in: visionImage)
@@ -89,5 +122,82 @@ public class OCRFrameProcessorPlugin: FrameProcessorPlugin {
             "boundingCenterX": frameRect.midX,
             "boundingCenterY": frameRect.midY
         ]
+    }
+}
+
+enum Orientation: String {
+    case portrait = "portrait"
+    case landscapeLeft = "landscapeLeft"
+    case portraitUpsideDown = "portraitUpsideDown"
+    case landscapeRight = "landscapeRight"
+}
+
+final class OCROrientationManager {
+    private let motionManager = CMMotionManager()
+    private let operationQueue = OperationQueue()
+
+    var orientation: Orientation {
+        didSet {
+            if oldValue != orientation {
+                print("Device Orientation changed from \(oldValue) -> \(orientation)")
+            }
+        }
+    }
+
+    init() {
+        orientation = .portrait
+        startDeviceOrientationListener()
+    }
+
+    deinit {
+        stopDeviceOrientationListener()
+    }
+
+    private func startDeviceOrientationListener() {
+        stopDeviceOrientationListener()
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 0.2
+            motionManager.startAccelerometerUpdates(to: operationQueue) { accelerometerData, error in
+                if let error {
+                    print("Failed to get Accelerometer data! \(error)")
+                }
+                if let accelerometerData {
+                    self.orientation = accelerometerData.deviceOrientation
+                }
+            }
+        }
+    }
+
+    private func stopDeviceOrientationListener() {
+        if motionManager.isAccelerometerActive {
+            motionManager.stopAccelerometerUpdates()
+        }
+    }
+}
+
+extension CMAccelerometerData {
+    var deviceOrientation: Orientation {
+        let acceleration = acceleration
+        let xNorm = abs(acceleration.x)
+        let yNorm = abs(acceleration.y)
+        let zNorm = abs(acceleration.z)
+
+        if zNorm > xNorm && zNorm > yNorm {
+            return .portrait
+        }
+
+        if xNorm > yNorm {
+            if acceleration.x > 0 {
+                return .landscapeRight
+            } else {
+                return .landscapeLeft
+            }
+        } else {
+            if acceleration.y > 0 {
+                return .portraitUpsideDown
+            } else {
+                return .portrait
+            }
+        }
     }
 }
